aed1832b4a0dde4df0c42e6b05fd7029e373fb91
Dmitry Kovyazin <it@brandup.online>
2024-08-05 22:18:09 +0700

    start interop

---
+++ examples/interoplib/Stubs/interoplib/interoplib_interoplib_Utilities.h
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef INTEROPLIB_INTEROPLIB_INTEROPLIB_UTILITIES_H
+#define INTEROPLIB_INTEROPLIB_INTEROPLIB_UTILITIES_H
+
+namespace interoplib
+{
+    namespace interoplib
+    {
+        struct Utilities
+        {
+            // Helper Functions to access fields of managed object
+            // Declaration of stubs. These functions are implemented by Interop code developers
+
+            static void NativeGetHardwareSerial( CLR_RT_TypedArray_UINT8 param0, HRESULT &hr );
+
+        };
+    }
+}
+
+#endif // INTEROPLIB_INTEROPLIB_INTEROPLIB_UTILITIES_H
---
+++ examples/interoplib/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace interoplib
+{
+    public class Utilities
+    {
+        private static byte[] _hardwareSerial;
+
+        /// <summary>
+        /// Gets the hardware unique serial ID (12 bytes).
+        /// </summary>
+        public static byte[] HardwareSerial
+        {
+            get
+            {
+                if (_hardwareSerial == null)
+                {
+                    _hardwareSerial = new byte[12];
+                    NativeGetHardwareSerial(_hardwareSerial);
+                }
+
+                return _hardwareSerial;
+            }
+        }
+
+        #region Stubs 
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void NativeGetHardwareSerial(byte[] data);
+
+        #endregion stubs 
+    }
+}
