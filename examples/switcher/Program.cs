9739b9c3979baa5e10eebf0b3fc5c9854241435f
Dmitry Kovyazin <it@brandup.online>
2024-08-07 20:39:17 +0700

    add toggler

--- examples/switcher/Program.cs
+++ examples/switcher/Program.cs
@@ -8,22 +8,28 @@ namespace switcher
 {
     public class Program
     {
-        static GpioController s_GpioController;
-
         public static void Main()
         {
             Debug.WriteLine("Hello from nanoFramework!");
 
+            var gpioController = new GpioController();
+
+            var togglersEnable = gpioController.OpenPin(3, PinMode.Output); // 15 - CLK_EN выставить в 0 - это разрешение тактировани€. ћожно один раз выставить при загрузке программы
+            var togglersRefresh = gpioController.OpenPin(8, PinMode.Output);   // 12 - LATCH/CS0
+            var togglersClk = gpioController.OpenPin(40, PinMode.Output);  // 33 - SPI_CLK на clk
+            var togglersValue = gpioController.OpenPin(39, PinMode.Input);  // 32 - DATA_SER_OUT/MISO на MISO
+
+            // enable toggler function
+            togglersEnable.Write(PinValue.Low);
+
+            // init PWM pins
             var count = 8;
             var pins = new int[] { 35, 36, 6, 7, 15, 48, 47, 21 };
             var leds = new PwmChannel[count];
-
-            s_GpioController = new GpioController();
-
             for (var i = 0; i < count; i++)
             {
                 var pin = pins[i];
-                Configuration.SetPinFunction(pin, GetPwmFunction(i));
+                Configuration.SetPinFunction(pin, GetPWMFunction(i));
                 var pwmPin = PwmChannel.CreateFromPin(pin, 1000, 0);
                 pwmPin.Start();
                 leds[i] = pwmPin;
@@ -31,11 +37,30 @@ namespace switcher
 
             while (true)
             {
+                // ensure toggler registers
+                togglersRefresh.Write(PinValue.Low);
+                togglersRefresh.Write(PinValue.High);
+
+                // read toggler state
+                var togglers = new bool[8];
+                for (var i = 7; i >= 0; i--)
+                {
+                    var pValue = togglersValue.Read();
+                    togglers[i] = pValue != PinValue.High;
+
+                    togglersClk.Write(PinValue.High);
+                    togglersClk.Write(PinValue.Low);
+                }
+
                 double value = 0;
                 while (value < 1)
                 {
                     for (var i = 0; i < count; i++)
+                    {
+                        if (togglers[i] != true)
+                            continue;
                         leds[i].DutyCycle = value;
+                    }
 
                     value += 0.05;
 
@@ -44,14 +69,22 @@ namespace switcher
 
                 value = 1;
                 for (var i = 0; i < count; i++)
+                {
+                    if (togglers[i] != true)
+                        continue;
                     leds[i].DutyCycle = value;
+                }
 
                 Thread.Sleep(1000);
 
                 while (value > 0)
                 {
                     for (var i = 0; i < count; i++)
+                    {
+                        if (togglers[i] != true)
+                            continue;
                         leds[i].DutyCycle = value;
+                    }
 
                     value -= 0.05;
 
@@ -60,13 +93,17 @@ namespace switcher
 
                 value = 0;
                 for (var i = 0; i < count; i++)
+                {
+                    if (togglers[i] != true)
+                        continue;
                     leds[i].DutyCycle = value;
+                }
 
                 Thread.Sleep(1000);
             }
         }
 
-        static DeviceFunction GetPwmFunction(int index)
+        static DeviceFunction GetPWMFunction(int index)
         {
             switch (index)
             {
